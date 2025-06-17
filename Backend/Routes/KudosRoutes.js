5f7de9d2b2fcfa944d914a8f4e145a939cef826a
Stecy Chirinda <schirinda@meta.com>
2025-06-17 12:15:19 -0700

    I added routes for the cards

--- Backend/Routes/KudosRoutes.js
+++ Backend/Routes/KudosRoutes.js
@@ -24,6 +24,7 @@ router.get('/', async (req, res) => {
     res.json(kudo_board);
 });
 
+
 router.post('/', async (req, res) => {
     const allowedCategories = ['Celebration','Thank You','Inspiration']
     if (!req.body.title || !allowedCategories.includes(req.body.category)){
@@ -51,4 +52,66 @@ router.delete('/:id',async(req,res)=>{
     })
     res.status(200).json(deletedBoard);
 })
+
+// Routing to cards
+
+router.get('/:boardId/cards', async (req,res) => {
+    const {boardId} = req.params;
+    try{
+    const cards = await prisma.Kudos_Board.findMany({
+        where: {boardId: parseInt(boardId)},
+    });
+    res.json(cards);
+    }catch(error){
+        res.status(500).json({message: 'Failed to fetch cards'});
+    }
+});
+
+router.get('/:boardId/cards/:id', async (req,res) => {
+    const {boardId,id} = req.params;
+    try{
+    const card = await prisma.Kudos_Board.findUnique({
+        where: {id: parseInt(id)},
+    });
+    res.json(card);
+    }catch(error){
+        res.status(500).json({message: 'Failed to fetch card'});
+    }
+    });
+
+router.post('/:boardId/cards', async (req, res) => {
+    const {boardId} = req.params;
+    if (!req.body.title || !req.body.description || !req.body.gif_url){
+        return res.status(400).json({message: 'Title, Description and gifUrl are required'});
+    }
+    const {title,description,gifUrl,autor,Kudos_count}=req.body;
+    let finalGifUrl = gifUrl;
+    if (!gifUrl){
+        finalGifUrl = await fetchRandomGif();
+    }
+    const newBoard = await prisma.Kudos_Board.create({
+        data: {
+            title,
+            description,
+            autor,
+            Kudos_count,
+            gif_url: finalGifUrl
+        }
+    });
+    res.status(201).json(newBoard);
+
+});
+
+router.delete('/:boardId/cards/:id',async(req,res)=>{
+    const{id} = req.params;
+    try{
+    const deletedCard = await prisma.Kudos_Board.delete({
+        where : {id: parseInt(id)}
+    })
+    res.status(200).json(deletedCard);
+    }catch(error){
+        res.status(500).json({message: 'Failed to delete card'});
+    }
+    })
+
 module.exports = router;
