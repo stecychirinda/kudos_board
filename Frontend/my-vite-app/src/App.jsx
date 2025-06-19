5cc36a5cddb70439c48b2dd399fadf6e29d9f46c
Stecy Chirinda <schirinda@meta.com>
2025-06-18 19:10:44 -0700

    Implemented toggle between dark mode and light mode

--- Frontend/my-vite-app/src/App.jsx
+++ Frontend/my-vite-app/src/App.jsx
@@ -3,14 +3,23 @@ import './App.css'
 import HomePage from './Components/HomePage'
 import BoardDisplayPage from './Components/BoardDisplayPage'
 import {Routes, Route, Router} from 'react-router-dom'
+import {useState} from 'react'
 
 const App = () => {
+  const [theme, setTheme] = useState('light')
+  const toggleTheme = () => {
+    setTheme(prevTheme=> (prevTheme === 'light' ? 'dark' : 'light'))
+  };
+
   return (
-    <div className="App">
+    <div className={`app-container ${theme}-mode`}>
         <Routes>
           <Route path="/" element={<HomePage />}/>
           <Route path="/:id" element={<BoardDisplayPage />}/>
         </Routes>
+        <button onClick={toggleTheme}>
+          Toggle to {theme === 'light' ? 'Dark' : 'Light'} Mode
+        </button>
     </div>
   )
 }
